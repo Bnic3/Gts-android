09525a3946b77c46725ddb3939ed37807cc255f6
Bnic3 <bnicemagnolia@gmail.com>
2017-07-31 13:59:26 +0100

    Iinitial commit with splash

---
+++ app/src/main/java/com/gts/gts/WelcomeActivity/Splash.java
@@ -0,0 +1,46 @@
+package com.gts.gts.WelcomeActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gts.gts.MainActivity;
+import com.gts.gts.R;
+
+public class Splash extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread splash = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }
+                 catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }//end run
+        };
+    }
+}
